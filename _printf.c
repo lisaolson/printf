4f317e0f26e58e2894ba3ac454541e17644f5f7e
Donnerschlag <andrew.suh.94@gmail.com>
2018-02-25 15:20:42 -0800

    Added stuff in the printf function and added struct in header

--- _printf.c
+++ _printf.c
@@ -5,4 +5,31 @@
 
 int _printf(const char *format, ...)
 {
+	int cnt = 0;
+	char *s;
+	buffer = malloc(1024 * sizeof(char));
+
+	va_list list;
+
+	print_list prog_list[] = {
+		{"s", print_string},
+		{"c", print_char}
+	};
+
+	va_start(list, format);
+
+	while (format[cnt] != '\0')
+	{
+		if (format[cnt] == '%')
+		{
+		}
+
+		buffer[buffer_count] = format[cnt];
+	}
+
+
+
+
+	write(1, buffer, cnt);
+	va_end(list);
 }
--- holberton.h
+++ holberton.h
@@ -1,6 +1,11 @@
 #ifndef holberton_h
 #define holberton_h
 
+typedef struct _printfs {
+	char *spec;
+	void (*p)(va_list);
+} print_list;
+
 int _printf(const char *format, ...);
 
 #endif
