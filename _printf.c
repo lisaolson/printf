13a74ad1e8edac71dbecb48ec26ced9cab4a6463
Donnerschlag <andrew.suh.94@gmail.com>
2018-02-25 12:10:25 -0800

    Made header and start of _printf function

---
+++ _printf.c
@@ -0,0 +1,8 @@
+#include "holberton.h"
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdarg.h>
+
+int _printf(const char *format, ...)
+{
+}
---
+++ holberton.h
@@ -0,0 +1,6 @@
+#ifndef holberton_h
+#define holberton_h
+
+int _printf(const char *format, ...);
+
+#endif
