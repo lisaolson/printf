bf3f59d2d125e525dac60944d01cae1b9d66e280
Andrew Suh <Donnerschlag@users.noreply.github.com>
2018-02-26 00:39:15 -0800

    Changed type and added program code

--- printstring.c
+++ printstring.c
@@ -1,6 +1,16 @@
 #include "holberton.h"
 
-void print_string(va_list list, *buffer, int a)
+int print_string(va_list list, *buffer, int a)
 {
+        int tmp = 0;
+        char *c;
 
+        c = va_arg(list, char *);
+        len = _strlen(c);
+        /*char *s = malloc (len * sizeof(char) + 1);
+        if (s == NULL)
+                return(NULL);*/
+        _strcpy(buffer, c, a);
+        free(s);
+        return (len);
 }
