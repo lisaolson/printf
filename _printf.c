dcfcd2bf0a85995de48c985b8c4a16281b5af354
Andrew Suh <Donnerschlag@users.noreply.github.com>
2018-02-26 00:39:55 -0800

    Added stuff and noted possible places to fix

--- _printf.c
+++ _printf.c
@@ -5,17 +5,11 @@
 
 int _printf(const char *format, ...)
 {
-	int cnt = 0, cnt2 = 0;
-	char *s;
+	int cnt = 0, cnt2 = 0, tmp = 0, buffer_count = 0, cnt = 0, str_len = 0;
+	char *s, *c, *str;
 	char *buffer = malloc(1024 * sizeof(char));
 
 	va_list list;
-
-	print_list prog_list[] = {
-		{"s", print_string},
-		{"c", print_char}
-		{NULL, NULL}
-	};
 
 	va_start(list, format);
 
@@ -23,21 +17,23 @@ int _printf(const char *format, ...)
 	{
 		if (format[cnt] == '%')
 		{
-			while (prog_list[cnt2].spec != NULL)
-			{
-				if (prog_list[cnt2].spec[0] == format[cnt])
-				{
-					prog_list[cnt2].p(list);
-				}
-			}
+			c = spec_get(format, cnt);
+			if (c == 0)
+				break;
+/*get rid of break and set conditional to not equal to and add the rest underneath inside the if*/
+			str = prog_get(c);
+			if (str == NULL)
+				break;
+/*get rid of break and set conditional to not equal to and add the rest underneath inside the if*/
+			str_len = prog_get(list, buffer, buffer_count)
+			buffer_count += str_len;
+			cnt += 2;
+
 		}
-
 		buffer[buffer_count] = format[cnt];
+		buffer_count++;
+		cnt++;
 	}
-
-
-
-
-	write(1, buffer, cnt);
+	write(1, buffer, buffer_count);
 	va_end(list);
 }
