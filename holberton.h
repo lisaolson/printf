0ac0727c3d364b0191b8abc22aa5a1a5f443a34b
Andrew Suh <Donnerschlag@users.noreply.github.com>
2018-02-25 16:44:18 -0800

    Added protoypes for string copy function and calloc

--- holberton.h
+++ holberton.h
@@ -7,5 +7,7 @@ typedef struct _printfs {
 } print_list;
 
 int _printf(const char *format, ...);
+char *_strcpy(char *dest, char *src);
+void *_calloc(unsigned int nmemb, unsigned int size);
 
 #endif
