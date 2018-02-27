cb5ad45303627713f0651574e3f45c8c070e564d
Andrew Suh <Donnerschlag@users.noreply.github.com>
2018-02-26 16:45:53 -0800

    initialized variable

--- spec_len.c
+++ spec_len.c
@@ -2,6 +2,7 @@
 
 int spec_len(const char *format, int i)
 {
+        int cnt = 0;
         for (cnt = 0; format[i + cnt] != '\0'; cnt++)
         {
                 if (format[i + cnt] <= 'z' && format[i + cnt] >= 'a')
