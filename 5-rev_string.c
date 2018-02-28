213c244811d358959c3d7d6b4a9e3ecc5c0d2553
Donnerschlag <andrew.suh.94@gmail.com>
2018-02-27 23:38:09 -0800

    changed logic by starting atlast

--- 5-rev_string.c
+++ 5-rev_string.c
@@ -7,35 +7,21 @@
 
 int rev_string(va_list list)
 {
-	int count = 0, rev = 0, counter = 0, cnt = 0;
-	char tmp, tmp2;
-	char *s, *tmp3;
+	int rev = 0, cnt = 0;
+	char *s;
 
 	s = va_arg(list, char *);
-	for (count = 0; s[count] != '\0'; count++)
+
+	for (cnt = 0; s[cnt] != '\0'; cnt++)
+	{
 		;
-	tmp3 = malloc(sizeof(char) * count);
-
-	for (cnt = 0; cnt < count; cnt++)
-	{
-		tmp3[cnt] = s[cnt];
 	}
-	count = count - 1;
-	counter = count;
+	cnt = cnt - 1;
 
-	for (rev = 0; rev < count; rev++)
+	for (rev = cnt; rev >= 0; rev--)
 	{
-		tmp2 = tmp3[rev];
-		tmp = tmp3[count];
-		tmp3[count] = tmp2;
-		tmp3[rev] = tmp;
-		count--;
+		_putchar(s[rev]);
 	}
 
-	for (cnt = 0; tmp3[cnt] != '\0'; cnt++)
-	{
-		_putchar(tmp3[cnt]);
-	}
-	free(tmp3);
-	return (counter);
+	return (cnt);
 }
